7b91b16766a02d375d5e6662ec5ab2493a761f42
Guilherme Morone Araujo <guimorone@gmail.com>
2022-02-05 00:11:31 -0300

    navbar feita, bootstrap incluido

--- challenge/myapp/views.py
+++ challenge/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+# Home page
 def index(request):
-    return HttpResponse('<h1>Hey, Welcome</h1>')
+    return render(request, 'index.html')
