124578175ef7d5e3e33aef367c2b45345cc488e2
Guilherme Morone Araujo <guimorone@gmail.com>
2022-02-05 11:55:24 -0300

    inconsistência aplicada

---
+++ challenge/myapp/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class PostMethod(forms.Form):
+    firstName = forms.CharField(max_length = 30)
+    lastName = forms.CharField(max_length = 50)
+    participation = forms.CharField(max_length = 5)
--- challenge/myapp/views.py
+++ challenge/myapp/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from myapp.forms import PostMethod
+from django.http import HttpResponseBadRequest
+import json
 
 # Home page
-def index(request):
-    return render(request, 'index.html')
-
 # Atualizar gráfico e tabela
-def addInfo(request):
-    text = request.POST['text']
+def index(request):
+    # GET
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    # POST
+    else:
+        form = PostMethod(request.POST)
+        if form.is_valid():
+            context = {
+                'form': form
+            }
+            return render(request, 'index.html', context = context)
+        # Inconsistência
+        else:
+            return HttpResponseBadRequest(json.dumps(form.errors), 
+                                          content_type = 'application/json')
